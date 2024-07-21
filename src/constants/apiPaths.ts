24944742fc00bdabc277ab2be02407c17f198df9
likhanna <khlr@list.ru>
2024-07-21 18:26:51 +0500

    task-8: Integration with SQL Database

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,7 @@ const API_PATHS = {
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://pqk7o5l18l.execute-api.eu-west-1.amazonaws.com/prod",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  cart: "https://628vcjfz3d.execute-api.eu-west-1.amazonaws.com/prod",
 };
 
 export default API_PATHS;
